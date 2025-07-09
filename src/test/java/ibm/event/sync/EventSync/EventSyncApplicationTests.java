a083c1637b424ef75ca8b75ee529f3fc9665e0be
Gvidas Kalvonis <gvidaskalv@gmail.com>
2025-07-09 16:09:37 +0300

    Initial commit

---
+++ src/test/java/ibm/event/sync/EventSync/EventSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.event.sync.EventSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
